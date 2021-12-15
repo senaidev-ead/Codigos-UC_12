964c1affb3b813e48a4a3d664ce656de7ce47443
rolfiluz <89606223+rolfiluz@users.noreply.github.com>
2021-12-15 15:10:43 -0300

    Add files via upload

---
+++ SA2/codigo_apostila_material_web/app/src/main/java/com/rh/app/funcionario/controller/funcionarioController.java
@@ -0,0 +1,29 @@
+package com.rh.app.funcionario.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.rh.app.funcionario.models.Funcionario;
+import com.rh.app.funcionario.repository.FuncionarioRepository;
+
+/** @author Rolfi Luz - Senai * */
+@Controller
+public class funcionarioController {
+
+	@Autowired
+	private FuncionarioRepository fr;
+
+	@RequestMapping(value = "/funcionario", method = RequestMethod.GET)
+	public String abrirfuncionario() {
+		return "funcionario/funcionario";
+	}
+
+	@RequestMapping(value = "/funcionario", method = RequestMethod.POST)
+	public String gravarfuncionario(Funcionario funcionario) {
+		fr.save(funcionario);
+		return "redirect:/funcionario";
+	}
+
+}
